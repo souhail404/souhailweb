f1afd603808e68656a78caaa2f5d137a3204c749
souhail404 <elabbouisouhail@gmail.com>
2023-09-06 00:42:25 +0200

    toggle dark mode

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
         <h1>I Build  Virtual Adventures.</h1>
       </div>
       <div className="flex flex-col gap-4">
-        <p className="2xl:text-lg">Meet the mind behind the pixels – I'm <span className="important-word">Souhail El Abbouyi</span>, a <span className="important-word">passionate</span> navigator of the digital realm. Over the last two years, I've honed my skills as a <span className="important-word">self-taught web developer</span>, all while pursuing my commerce and management master's degree. I've discovered my true calling in crafting virtual experiences that <span className="important-word">bridge the gap between code and creativity.</span></p>
+        <p className="2xl:text-lg"> Meet the mind behind the pixels – I'm <span className="important-word">Souhail El Abbouyi</span>, a <span className="important-word">passionate</span> navigator of the digital realm. Over the last two years, I've honed my skills as a <span className="important-word">self-taught web developer</span>, all while pursuing my commerce and management master's degree. I've discovered my true calling in crafting virtual experiences that <span className="important-word">bridge the gap between code and creativity.</span></p>
         {/* <p className="2xl:text-lg"> At 20, my journey began with a curious exploration of HTML, CSS, and JavaScript – where each line of code felt like a brushstroke on a digital canvas. Delving into the depths of the MERN stack and beyond, I've grown to build dynamic websites that seamlessly blend functionality with design.</p> */}
         {/* <p className="2xl:text-lg">anyways <Link href="" className="underline">let's build something together.</Link></p> */}
       </div>
