2701eb9eeb6da941c0b7a6163a306d07689c0889
souhail404 <elabbouisouhail@gmail.com>
2023-09-06 01:50:28 +0200

    rebuild

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
-import {AiFillLinkedin, AiOutlineArrowRight, AiOutlineEllipsis, AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter} from 'react-icons/ai'
-import { MdOutlinePersonOutline } from "react-icons/md";
+import {AiFillLinkedin, AiOutlineGithub, AiOutlineInstagram, AiOutlineTwitter} from 'react-icons/ai'
 export default function Home() {
   return (
     <section className="flex flex-col  midtall:gap-3 tall:gap-[4vh] my-auto">
@@ -10,7 +9,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col gap-4">
         <p className="2xl:text-lg"> Meet the mind behind the pixels – I'm <span className="important-word">Souhail El Abbouyi</span>, a <span className="important-word">passionate</span> navigator of the digital realm. Over the last two years, I've honed my skills as a <span className="important-word">self-taught web developer</span>, all while pursuing my commerce and management master's degree. I've discovered my true calling in crafting virtual experiences that <span className="important-word">bridge the gap between code and creativity.</span></p>
-        
+
       </div>
       <div className="flex flex-col sm:flex-row gap-4 my-4">
         <Link className="cta-btn secondary"  href={`about`}>About Me  </Link>
