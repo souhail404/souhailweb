81a94b89be4dc81cae13cd80d3d04c8dabf90a89
souhail404 <elabbouisouhail@gmail.com>
2023-09-06 04:44:15 +0200

    toggle mobile menu

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -6,19 +6,10 @@ import { BsCircleHalf } from "react-icons/bs"
 const ThemeToggler = ()=>{
     const {theme, setTheme} = useTheme();
     const [isDark, setIsDark] = useState(false);
-    const [mounted , setMounted]= useState(false);
 
-    useEffect(()=>{
-        setMounted(true)
-    },[])
-    
     useEffect(()=>{
         isDark ? setTheme('dark'): setTheme('light')
     },[isDark])
-    
-    if (!mounted) {
-        return null
-    }
     
     return(
         <button type="button" className="header-icon" onClick={()=>setIsDark(!isDark)}>
